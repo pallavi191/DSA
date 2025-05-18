678672b9c78bb3490f7b2949b46d8479523bf078
Pallavi Patil <72797772+pallavi191@users.noreply.github.com>
2025-05-19 00:28:02 +0530

    Create palindrome-linked-list.cpp

---
+++ linkedlist/palindrome-linked-list.cpp
@@ -0,0 +1,49 @@
+class Solution {
+public:
+    bool isPalindrome(ListNode* head) {
+        ListNode *temp = head;
+        int count = 0;
+
+        while(temp) {
+            count++;
+            temp = temp -> next;
+        }
+
+        if(count == 1)
+        return 1;
+
+        count /= 2;
+        ListNode *prev = NULL, *curr = head;
+
+        while(count--) {
+            prev = curr;
+            curr = curr -> next;
+        }        
+
+        prev -> next = NULL;
+
+        ListNode *front = NULL;
+
+        prev = NULL;
+
+        while(curr) {
+            front = curr -> next;
+            curr -> next = prev;
+            prev = curr;
+            curr = front;
+        }
+
+        ListNode *head1 = head, *head2 = prev;
+
+        while(head1) {
+            if(head1 -> val != head2 -> val)
+            return 0;
+            else {
+                head1 = head1 -> next;
+                head2 = head2 -> next;
+            }
+        }
+
+        return 1;
+    }
+};
