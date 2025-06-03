f4ea2765b3799b93fc75fdffb21dfd08fe06da3b
Pallavi Patil <72797772+pallavi191@users.noreply.github.com>
2025-06-03 17:20:42 +0530

    Create create-tree-without-recursion.cpp

---
+++ binary tree/create-tree-without-recursion.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <queue>
+
+using namespace std;
+class Node {
+    public:
+    Node *left, *right;
+    int val;
+    Node(int x) {
+        left = right = NULL;
+        val = x;
+    }
+};
+
+int main() {
+    queue<Node*> q;
+
+    int first, second;
+
+    Node* root = NULL;
+
+    Node *temp = new Node(1);
+    root = temp;
+    q.push(temp);
+
+    while(!q.empty()) {
+        temp = q.front();
+        cout << "Enter left child of " << temp -> val << endl;
+        cin >> first;
+
+        if(first > -1) {
+            temp -> left = new Node(first);
+            q.push(temp -> left);
+        }
+
+        cout << "Enter right child of " << temp -> val << endl;
+        cin >> second;
+
+        if(second > -1) {
+            temp -> right = new Node(second);
+            q.push(temp -> right);
+        }
+
+        q.pop();
+    }
+    return 0;
+}
