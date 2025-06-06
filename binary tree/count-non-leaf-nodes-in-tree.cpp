bf86103ebae796022a23471f6846ca20443b6a05
Pallavi Patil <72797772+pallavi191@users.noreply.github.com>
2025-06-06 07:30:54 +0530

    Create count-non-leaf-nodes-in-tree.cpp

---
+++ binary tree/count-non-leaf-nodes-in-tree.cpp
@@ -0,0 +1,23 @@
+// https://www.geeksforgeeks.org/problems/count-non-leaf-nodes-in-tree/1?page=1&difficulty%5B%5D=-1&category%5B%5D=Tree&sortBy=submissions
+
+/*Structure of the node of the tree is as
+struct Node
+{
+    int data;
+    struct Node* left;
+    struct Node* right;
+};
+*/
+// you are required to complete this function
+// the function should return the count of Non-Leaf Nodes
+class Solution {
+  public:
+    int countNonLeafNodes(Node* root) {
+        // Code here
+        if(root == NULL) return 0;
+
+        if(!root -> left && !root -> right) return 0;
+
+        return 1 + countNonLeafNodes(root -> left) + countNonLeafNodes(root -> right);    
+    }
+};
