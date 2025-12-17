3df6bacd67b2c7dbe25e83be6b5e602278ca1acc
Pallavi Patil <72797772+pallavi191@users.noreply.github.com>
2025-12-17 18:54:09 +0530

    Add hybrid inheritance example in hybrid.cpp
    Implement hybrid inheritance with Vehicle, PetrolCar, ElectricCar, and Car classes. Demonstrate constructor calls and display method.

---
+++ OOPs/hybrid.cpp
@@ -0,0 +1,85 @@
+// #hybrid Level Inheritance
+// | Inheritance Type | Present |
+// | ---------------- | ------- |
+// | Single           | ✔       |
+// | Multiple         | ✔       |
+// | Multilevel       | ✔       |
+// | **Hybrid**       | ✔       |
+
+//           Vehicle
+//          /       \
+//   PetrolCar   ElectricCar
+//          \       /
+//             Car
+
+
+#include <iostream>
+using namespace std;
+
+// Base class
+class Vehicle {
+protected:
+    int speed;
+public:
+    Vehicle(int speed) : speed(speed) {
+        cout << "Vehicle constructor called\n";
+    }
+
+    void display() {
+        cout << "Speed: " << speed << endl;
+    }
+};
+
+// Derived class 1 (virtual inheritance)
+class PetrolCar : virtual public Vehicle {
+protected:
+    int fuelLevel;
+public:
+    PetrolCar(int speed, int fuel)
+        : Vehicle(speed), fuelLevel(fuel) {
+        cout << "PetrolCar constructor called\n";
+    }
+};
+
+// Derived class 2 (virtual inheritance)
+class ElectricCar : virtual public Vehicle {
+protected:
+    int batteryLevel;
+public:
+    ElectricCar(int speed, int battery)
+        : Vehicle(speed), batteryLevel(battery) {
+        cout << "ElectricCar constructor called\n";
+    }
+};
+
+// Derived class 3 (Hybrid inheritance)
+class Car : public PetrolCar, public ElectricCar {
+public:
+    Car(int speed, int fuel, int battery)
+        : Vehicle(speed),        // ✅ Virtual base initialized HERE
+          PetrolCar(speed, fuel),
+          ElectricCar(speed, battery) {
+        cout << "Car constructor called\n";
+    }
+
+    void display() {
+        Vehicle::display(); // if you trying to call without virtual it will hit ambiguos err
+        cout << "Fuel Level: " << fuelLevel << endl;
+        cout << "Battery Level: " << batteryLevel << endl;
+    }
+};
+
+int main() {
+    Car c(120, 50, 80);
+    c.display();
+    return 0;
+}
+
+// output
+// Vehicle constructor called
+// PetrolCar constructor called
+// ElectricCar constructor called
+// Car constructor called
+// Speed: 120
+// Fuel Level: 50
+// Battery Level: 80
